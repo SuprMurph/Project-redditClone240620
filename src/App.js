3057632ef1e3d3a1ee692b64b03964fb4164dd55
Ethan Murphy <emurphynanton@gmail.com>
2020-06-24 15:23:00 +0100

    Moved the component dir into src

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar.js';
 
 const App = () => {
   return (
     <React.Fragment className='mainPage'>
       <h1>Hello World</h1>
+      <Navbar/>
     </React.Fragment>
   );
 }
