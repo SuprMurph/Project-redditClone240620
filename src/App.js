9b674bd41c4938479da5518bbd96fc8610874e87
Ethan Murphy <emurphynanton@gmail.com>
2020-06-25 12:26:05 +0100

    Multiple changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar.js';
+import Newscard from './components/NewsCard';
 
 
 const App = () => {
@@ -8,9 +9,13 @@ const App = () => {
     <React.Fragment className='mainPage'>
       <Navbar/>
       
-      <h4 id='trendingTitle'>Trending today:</h4>
+      <div id='trendingToday'>
+        
+        <h4>Trending today:</h4>
 
-    
+        <Newscard titleText="Here's a Title" cardText="Here is some text"/>
+      </div>
+
     </React.Fragment>
   );
 }
