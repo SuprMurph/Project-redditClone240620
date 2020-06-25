147217a173e8bdb0a27f7d8e2ebefeb68bc6b8d7
MayabaH <52009296+MayabaH@users.noreply.github.com>
2020-06-25 16:23:18 +0100

    Side bar and main posts

---
+++ src/components/SubCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class SubCard extends React.Component {
+  render() {
+    return (
+      <div className="sub-info">
+        <h4>Today's Top Growing Communities</h4>
+        <ul>
+         <li></li>
+         <li></li>
+         <li></li>
+         <li></li>
+        </ul>
+      </div>
+    );
+  }
+}
+export default SubCard;
