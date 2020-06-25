4984ea27823d039fb5f3659e8ab5c00db81e0ad5
Ethan Murphy <emurphynanton@gmail.com>
2020-06-25 11:04:17 +0100

    new image for both

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './Navbar.css';
+import logo from '../images/redditLogo.png';
+import logoText from '../images/reddittext.png';
 
 const Navbar = () => {
     return (
         <div className='navbarWrapper'>
-            <img src='../images/redditLogo.png' height='50px' width='50px' alt='RedditRobot logo'/>
+            <img id='roboLogo' src={logo} width='50px' height='50px' alt='reddit robot logo'/>
+            <img id='logoText' src={logoText} width='100px' height='20px' alt='reddit robot logo'/>
+            <input type='text' placeholder='Search'></input>
+            <button id='login'>LOG IN</button>
+            <button id='signup'>SIGN UP</button>
+            <button id='rightDropDown'>needs an image</button>
         </div>
     )
 }
