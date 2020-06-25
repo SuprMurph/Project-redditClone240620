4b8f665e7f93df120247c5fc8f90137fc13568ad
Ethan Murphy <emurphynanton@gmail.com>
2020-06-25 10:41:51 +0100

    More images

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import './Navbar.css';
 const Navbar = () => {
     return (
         <div className='navbarWrapper'>
-            
+            <img src='../images/redditLogo.png' height='50px' width='50px' alt='RedditRobot logo'/>
         </div>
     )
 }
