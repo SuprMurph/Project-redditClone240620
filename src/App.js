81d0a160ac9ec9d4ccf7fdf6d455462cdbf32057
Ethan Murphy <emurphynanton@gmail.com>
2020-06-25 12:12:42 +0100

    trying to sort basics

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar.js';
-import NewsCard from './components/NewsCard.js';
+
 
 const App = () => {
   return (
     <React.Fragment className='mainPage'>
       <Navbar/>
       
-      <div>
-        <h4>Trending today:</h4>
-      </div>
-      
-      <NewsCard titleText='blah blah blah' cardText='blabity blibbity blooblah'/>
+      <h4 id='trendingTitle'>Trending today:</h4>
+
     
     </React.Fragment>
   );
