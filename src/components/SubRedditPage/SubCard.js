96a8cb691998c8f83bfe7213faed6c2ca4752712
Ethan Murphy <emurphynanton@gmail.com>
2020-06-26 15:38:53 +0100

    some changes

--- src/components/SubRedditPage/SubCard.js
+++ src/components/SubRedditPage/SubCard.js
@@ -6,31 +6,21 @@ class SubCard extends React.Component {
       <div className="sub-info">
         <a><h4 >Today's Top Growing Communities</h4></a>
         <ol>
-        <a><li id="top-li"> <img className="list-image"src="https://styles.redditmedia.com/t5_2yuqy/styles/communityIcon_8q8ca4zwx7921.jpg?width=256&format=pjpg&s=3acd595235e914b95402bf0471ce990e98854736 " /> r/TrueoffMyChest</li></a>
-        <a><li > <img className="list-image"src="https://b.thumbs.redditmedia.com/6EKfzU5PYmvE4USNgMZaBR6iCS5NnJ3YFTkZyPbXnZM.png" /> r/Thelastofus</li></a>
-        <a><li > <img className="list-image"src="https://styles.redditmedia.com/t5_2t7gp/styles/communityIcon_xmne5k0j68231.png?width=256&s=3939074f1b3b20f9877759b588a54ac7ea08d1e5" /> r/Funniest TrueoffMyChest</li></a>
-        <a><li > <img className="list-image"src="https://styles.redditmedia.com/t5_2szyo/styles/communityIcon_x3ag97t82z251.png?width=256&s=33531dceba6466953aadef3073f36cfc2e267175" /> r/Whoisthis</li></a>
-
+          <a><li id="top-li"> <img className="list-image"src="https://styles.redditmedia.com/t5_2yuqy/styles/communityIcon_8q8ca4zwx7921.jpg?width=256&format=pjpg&s=3acd595235e914b95402bf0471ce990e98854736 " /> r/TrueoffMyChest</li></a>
+          <a><li > <img className="list-image"src="https://b.thumbs.redditmedia.com/6EKfzU5PYmvE4USNgMZaBR6iCS5NnJ3YFTkZyPbXnZM.png" /> r/Thelastofus</li></a>
+          <a><li > <img className="list-image"src="https://styles.redditmedia.com/t5_2t7gp/styles/communityIcon_xmne5k0j68231.png?width=256&s=3939074f1b3b20f9877759b588a54ac7ea08d1e5" /> r/Funniest TrueoffMyChest</li></a>
+          <a><li > <img className="list-image"src="https://styles.redditmedia.com/t5_2szyo/styles/communityIcon_x3ag97t82z251.png?width=256&s=33531dceba6466953aadef3073f36cfc2e267175" /> r/Whoisthis</li></a>
+          <a><li > <img className="list-image"src="https://styles.redditmedia.com/t5_2szyo/styles/communityIcon_x3ag97t82z251.png?width=256&s=33531dceba6466953aadef3073f36cfc2e267175" /> r/Politics</li></a>
         </ol>
-     <a> <button id="view-all"> VIEW ALL</button></a>
-<<<<<<< HEAD
-     <div className="bottomBtns">
-      <button>Sports</button>
-      <button>News</button>
-      <button>Gaming</button>
-      <button>Aww</button>
-     </div> 
+      <a> <button id="view-all"> VIEW ALL</button></a>
+      <div className="bottom-buttons">
+        <a ><button className="bbutton">Sports</button></a>
+        <a ><button className="bbutton">News</button></a>
+        <a ><button className="bbutton">Gaming</button></a>
+        <a ><button className="bbutton" >Aww</button></a>
+      </div>
   </div>
   
-=======
-     <div className="bottom-buttons">
-  <a className="bbutton"><button id="1">Sports</button></a>  
-  <a className="bbutton"><button id="2">News</button></a>
-  <a className="bbutton"><button id="3" >Gaming</button></a>
-  <a className="bbutton"><button id="4">Aww</button></a>
-  </div>
- 
->>>>>>> 789442733fc518c9410d29ecbe14ec0e7e8ab4be
 
       
     );
